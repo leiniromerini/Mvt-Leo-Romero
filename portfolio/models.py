84854e16d55079fec37476172e7d945d43db3ab4
Leonardo Jaziel Romero Flores <leiniromerini@MacBook-Pro-de-Leonardo.local>
2022-05-30 20:46:24 -0500

    first commit

---
+++ portfolio/models.py
@@ -0,0 +1,20 @@
+from email.mime import image
+from nturl2path import url2pathname
+from turtle import title
+from django.db import models
+
+from django.db import models
+from django.db.models.fields import CharField, DateField, URLField
+from django.db.models.fields.files import ImageField
+from datetime import date
+
+
+class Project(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
+    image = ImageField(upload_to="portfolio/images")
+    url = URLField(blank=True)
+    date = DateField(default=date.today)
+
+    def __str__(self) -> str:
+        return self.title
